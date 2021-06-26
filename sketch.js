28540852ae0baefd4ea4d4e4eb30f2b2b090f564
RaghavPandit <rp20020918@gmail.com>
2021-06-26 21:34:27 +0530

    Doing something

--- sketch.js
+++ sketch.js
@@ -1,8 +1,25 @@
-function setup(){
-    createCanvas(400,400);
-    background(155);
+var cols = 5;
+var rows = 5;
+var grid = new Array();
+var openSet = [];
+var closedSet = [];
+var start;
+var end;
+
+
+function setup() {
+    createCanvas(400, 400);
+    for (var i = 0; i < cols; i++) {
+        grid[i] = new Array(rows);
+    }
+    for (var i = 0; i < cols; i++) {
+        for (var j = 0; j < cols; j++) {
+            grid[i] = new Array(rows);
+        }
+    }
+    console.log(grid);
 }
 
-function draw(){
-
+function draw() {
+    background(0);
 }
